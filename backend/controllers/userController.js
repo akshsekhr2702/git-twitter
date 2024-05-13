705226a4e69f79f3d841a409036f1cccdc63ce45
AKSHYANSHU <akshyansunayak360985@gmail.com>
2024-05-13 22:23:24 +0530

    updateProfile✅ and worked freshly with cloudinary

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,8 @@
+import  bcrypt  from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary";
+//models
 import Notification from "../models/notifiation.model.js";
 import User from "../models/userModel.js";
-
 export const getUserProfile = async(req,res)=>{
     const {username} = req.params;
 
@@ -84,4 +86,69 @@ export const getSuggestedUsers = async(req, res)=>{
         res.status(500).json({error: error.message})
         
     }
+}
+
+export const updateUser = async(req, res)=>{
+   let {fullname, email, username, currentPassword, newPassword, bio, link} = req.body;
+   let{profileImg, coverImg} = req.body;
+
+   const userId = req.user._id; // Assuming the userId is sent from the client-side as "userId"
+
+
+   try {
+        let user = await User.findById(userId);
+        if(!user) return res.status(404).json({ message: "User not found"});
+
+        if((!newPassword && currentPassword )||(!currentPassword && newPassword)){
+            return res.status(401).json({
+                error: "Please provide both current password and new password"
+            })
+
+        }
+        if(currentPassword && newPassword){
+            const isMatch = await bcrypt.compare(currentPassword, user.password);
+            if(!isMatch) return res.status(404).json({
+                error:" current password is incorrect"
+            })
+            const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(newPassword, salt);
+        }
+        if(profileImg) {
+
+            if(user.profileImg){
+                await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0])
+            }
+
+            const uploadResponse = await cloudinary.uploader.upload(profileImg);
+            profileImg = uploadResponse.secure_url;
+        }
+        if(coverImg){
+            if(user.profileImg){
+                await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0])
+            }
+            const uploadResponse = await cloudinary.uploader.upload(coverImg);
+            coverImg = uploadResponse.secure_url;
+        }
+
+        user.fullname = fullname || user.fullname;
+        user.email = email|| user.email;
+        user.username = username || user.username;
+        user.bio = bio ||user.bio;
+        user.link  =  link|| user.link;
+        user.profileImg  = profileImg || user.profileImg;
+        user.coverImg  = coverImg || user.coverImg;
+        
+        user = await user.save();
+        //passwrod is null in response 
+        user.password = null;
+
+
+        return res.status(200).json(user);
+   } catch (error) {
+    console.log("Error in updateuser: ", error.message);
+    res.status(500).json({
+        error: error.message
+    })
+    
+   }
 }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollow, getSuggestedUsers, getUserProfile } from "../controllers/userController.js";
+import { followUnfollow, getSuggestedUsers, getUserProfile, updateUser } from "../controllers/userController.js";
 
 
 const router = express.Router();
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get("/profile/:username",protectRoute, getUserProfile);
 router.get("/suggested", protectRoute,getSuggestedUsers);
 router.post("/follow/:id", protectRoute,followUnfollow);
-// router.post("/suggested", protectRoute ,updateUserProfile);
+router.post("/update", protectRoute ,updateUser);
 
 export default router;
